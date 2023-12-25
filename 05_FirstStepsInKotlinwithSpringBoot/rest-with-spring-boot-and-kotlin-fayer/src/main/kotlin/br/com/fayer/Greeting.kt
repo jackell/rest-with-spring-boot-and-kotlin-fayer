2c7bc35921b09875d12aeb5a8dfd0d1494345b5d
Jackell Alves <jackellalves@gmail.com>
2023-12-25 16:23:56 -0300

    First Steps in Kotlin with Spring Boot

---
+++ 05_FirstStepsInKotlinwithSpringBoot/rest-with-spring-boot-and-kotlin-fayer/src/main/kotlin/br/com/fayer/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.fayer
+
+class Greeting (val id: Long, val content: String)
---
+++ 05_FirstStepsInKotlinwithSpringBoot/rest-with-spring-boot-and-kotlin-fayer/src/main/kotlin/br/com/fayer/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.fayer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
